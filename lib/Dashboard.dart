70bb17d8770d0f5770c382138561640cbeabad1c
cailavillostas <cailavillostas@gmail.com>
2025-07-15 09:45:41 +0800

    updates

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -233,6 +233,7 @@ class _AddTripPageState extends State<AddTripPage> {
                     decoration: const InputDecoration(labelText: 'Destination'),
                     validator: (v) => v!.isEmpty ? 'Required' : null,
                   ),
+
                   const SizedBox(height: 12),
                   TextFormField(
                     controller: _startDateController,
